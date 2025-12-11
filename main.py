abada23be29d70b2c69ba514f73ffe94f914fd83
elf-lord <krylov.grigory+github@gmail.com>
2025-12-11 09:27:36 +0300

    cia is there

---
+++ main.py
@@ -0,0 +1,92 @@
+import curses
+from curses import window
+import traceback
+import json
+import os
+
+class Choice:
+    def __init__(self, next_id: int, answer: str):
+        self.next_id = next_id
+        self.answer = answer
+
+class DialogueNode:
+    def __init__(self, info: dict):
+        self.id = info['id']
+        self.character = info['character']
+        self.phrase = info['phrase']
+        self.choices: list[Choice] = list()
+        for c in info['choices']:
+            self.choices.append(Choice(c['next_id'], c['answer']))
+
+
+def read_dialogues_from_file(filename: str) -> dict[int, DialogueNode]:
+    dialogues_json: json = None
+    with open(filename, 'r') as fp:
+        dialogues_json = json.load(fp)
+
+    dialogues: dict[int, DialogueNode] = dict()
+    for d in dialogues_json:
+        dialogues[d['id']] = DialogueNode(d)
+
+    return dialogues
+
+
+def curses_begin(stdscr: window):
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+    curses.curs_set(False)
+
+
+def curses_end(stdscr: window):
+    stdscr.keypad(False)
+    curses.echo()
+    curses.nocbreak()
+    curses.curs_set(True)
+    curses.endwin()
+
+
+def main(stdscr: window):
+    curses_begin(stdscr)
+    window_should_close = False
+
+    dialogues: dict[int, DialogueNode]
+    try:
+        dialogues = read_dialogues_from_file('dialogues.json')
+    except json.decoder.JSONDecodeError as e:
+        print(f'Failed to read the dialogue file: {e}')
+        return
+
+    current_node: DialogueNode = dialogues[0]
+
+    while not window_should_close:
+        stdscr.clear()
+        stdscr.addstr(current_node.character, curses.A_BOLD)
+        stdscr.addstr('\n')
+        stdscr.addstr(current_node.phrase)
+        stdscr.addstr('\n')
+        stdscr.addstr('-' * 10)
+        stdscr.addstr('\n')
+        for i, choice in enumerate(current_node.choices):
+            stdscr.addstr(f'{i + 1}: {choice.answer}\n')
+
+        c = chr(stdscr.getch())
+        if c == 'q':
+            window_should_close = True
+        if c.isdecimal() and int(c) >= 1 and int(c) <= len(current_node.choices):
+            choice = current_node.choices[int(c) - 1]
+            current_node = dialogues[choice.next_id]
+
+        stdscr.refresh()
+
+    curses_end(stdscr)
+
+
+if __name__ == '__main__':
+    stdscr = curses.initscr()
+    try:
+        main(stdscr)
+    except Exception as e:
+        curses_end(stdscr)
+        print(f'An error occured: {e}')
+        traceback.print_exc()
